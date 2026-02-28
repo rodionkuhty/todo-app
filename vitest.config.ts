6b4cbf9fdd42392a86dc74f677d588b050156065
rodionkuhty <rodionkuhty@gmail.com>
2026-02-28 19:29:09 +0300

    add coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,11 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      exclude: ['src/components/ui'],
+    },
   },
   resolve: {
     alias: {
