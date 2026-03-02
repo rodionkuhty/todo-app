44382f96276c56cdef7a622ab1ceaf8b1534c781
rodionkuhty <rodionkuhty@gmail.com>
2026-03-02 18:33:04 +0300

    add new libs

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+export function Home() {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-full'>
+      <h1 className='text-4xl font-bold mb-4'>Welcome to Todo App</h1>
+      <p className='text-lg text-muted-foreground'>
+        Manage your tasks efficiently
+      </p>
+    </div>
+  );
+}
+
+export default Home;
