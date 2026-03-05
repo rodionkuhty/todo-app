6bf5fe48fffd481a098b49d48d0bc9ee9a34d1b8
rodionkuhty <rodionkuhty@gmail.com>
2026-03-05 12:38:23 +0300

    refactor header

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,20 +2,39 @@ import { UsersAria } from '@/components/UserAria';
 import {
   NavigationMenu,
   NavigationMenuItem,
+  NavigationMenuLink,
   NavigationMenuList,
+  navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu';
 import { Link } from 'react-router-dom';
+import { cn } from '@/lib/utils.ts';
 
 export default function Menu() {
   return (
-    <div className='flex justify-between'>
+    <div className='flex items-center justify-between'>
       <NavigationMenu>
         <NavigationMenuList>
           <NavigationMenuItem>
-            <Link to='/'>Home</Link>
+            <NavigationMenuLink
+              asChild
+              className={cn(
+                navigationMenuTriggerStyle(),
+                'text-foreground transition-colors',
+              )}
+            >
+              <Link to='/'>Home</Link>
+            </NavigationMenuLink>
           </NavigationMenuItem>
           <NavigationMenuItem className='hidden md:flex'>
-            <Link to='/tasks'>Tasks</Link>
+            <NavigationMenuLink
+              asChild
+              className={cn(
+                navigationMenuTriggerStyle(),
+                'text-foreground transition-colors',
+              )}
+            >
+              <Link to='/tasks'>Tasks</Link>
+            </NavigationMenuLink>
           </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
